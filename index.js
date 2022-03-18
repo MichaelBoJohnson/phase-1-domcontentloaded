6df4405f15595c3481aac6434fb81dd4d121dba2
Michael Johnson <michaelbojohnson@gmail.com>
2022-03-18 16:10:29 -0500

    commit tree df8d09357e3a3ac8e5575929f86b5129d819d3f4

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+// set up DOMContentLoaded event listener
+document.addEventListener("DOMContentLoaded", function (){
+    
+    // use the events callback function to target the paragraph element with id="text" 
+
+    const newPText = document.getElementById('text');
+   
+   //replace text with "This si really cool!" using textContent
+    newPText.textContent = "This is really cool!";
+
+
+
+});
+
+
